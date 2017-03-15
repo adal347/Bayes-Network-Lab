c267bc11cdb5d898dd615657f64991b421e47ac1
adal <adita0898@gmail.com>
2017-03-15 02:17:26 +0000

    Try 2

--- bayesNetwork.cpp
+++ bayesNetwork.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <string>
 #include <cstring>
+#include <cstdlib>
 #include <cstdio>
 #include <sstream>
 #include <algorithm>
 #include <map>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
+typedef pair<string,double> P;
+
 /**
  * Struct Node to add nodes at the Bayesian Network
+ *
  * Attributes
  * ----------
  * Parents: The parents of the node. For 0 or N-parents;
@@ -17,106 +23,204 @@ using namespace std;
  * Probability_table: Probability table for actual node. Key is X in P(X).
  * Name: Name of the node.
  */
-struct node{
+struct node {
 	std::vector<struct node*> parents;
-	std::map<string, double> probabilityTable;
+	std::map<string, double> probability_table;
 	string name;
 };
 
+/**
+ * Function to create the nodes for the bayesian network
+ *
+ * Attributes
+ * ----------
+ * Nodes : we add the node in map to know what are the nodes
+ * Buffer : to retrieve the input
+ */
+void create_nodes(map<string, struct node*> &nodes, string buffer) {
+
+  // The builder to build tha name string.
+  string builder;
+  // Retrieve the line.
+	getline(cin, buffer);
+
+  for(int i = 0; i < buffer.length(); i++){
+    // if we find ',' we add the node
+    if(buffer[i]==',') {
+      nodes[builder] = new node;
+			nodes[builder]->name = builder;
+    } else if(i == (buffer.length() - 1)) {
+      // else if is the end we add the last node
+			builder += buffer[i];
+			nodes[builder] = new node;
+			nodes[builder]->name = builder;
+    } else if(buffer[i]==' ') {
+			builder="";
+		} else {
+			builder += buffer[i];
+		}
+	}
+}
+
+/**
+ * Function to create bayes network
+ *
+ * Attributes
+ * ----------
+ * Nodes : The map of nodes to add their Probabilities.
+ * Buffer :  to retrieve the input.
+ */
+void create_bayes_network(map<string, struct node*> &nodes, string buffer) {
+
+  string builder, aux;
+	char devourer;
+	double readerProb;
+
+	while(buffer.compare("") != 0) {
+    getline(cin, buffer);
+		builder = "";
+		bool indexNode = false;
+    for(int i = 0; i < buffer.length(); i++) {
+      if(buffer[i] == '|'){
+        // if find '|' the node have a parent
+        indexNode = true;
+				aux = builder;
+				builder="";
+			}
+      if(buffer[i] == '+' || buffer[i] == '-' || buffer[i] == ' ' || buffer[i] == '=' || buffer[i] == ',' || buffer[i] == '|' || (buffer[i] >= 48 && buffer[i] <= 57));
+			else{
+        // Start to construct the string key for the map of nodes
+        builder += buffer[i];
+			}
+      // To make the conection in base of the dependece
+			if(indexNode) {
+        if(buffer[i] == ',') {
+          // To create N parents
+          (nodes[aux])->parents.push_back(nodes[builder]);
+					builder="";
+        } else if (buffer[i] == '=') {
+          // If there is only one parent
+					(nodes[aux])->parents.push_back(nodes[builder]);
+					builder="";
+				}
+			}
+			if(buffer[i] == '=') {
+        if(!indexNode){
+          // The node is root P(X) is independient
+					aux=builder;
+				}
+        // Finish the creation of the edges
+				break;
+			}
+		}
+
+    //erase duplicated nodes
+    sort( (nodes[aux])->parents.begin(), (nodes[aux])->parents.end() );
+		(nodes[aux])->parents.erase( unique( (nodes[aux])->parents.begin(), (nodes[aux])->parents.end() ), (nodes[aux])->parents.end() );
+
+		// Create the probability table
+		builder="";
+		for(int i = 0; i < buffer.length(); i++) {
+			if(buffer[i] == '=') {
+				break;
+			} else if(buffer[i] == ' ' && buffer[i + 1] == '=');
+        // To ignore the last blank space " ="
+			else {
+				builder+=buffer[i];
+			}
+		}
+
+		if(builder.compare("") != 0) {
+			stringstream extract(buffer);
+			do {
+        // To eat all chars
+				extract >> devourer;
+			} while(devourer!='=');
+
+			extract >> readerProb;
+
+      // Store the true values P(+X|Y)
+			(nodes[aux])->probability_table[builder]=readerProb;
+      // Store the true values P(-X|Y)
+			builder[0]='-';
+			(nodes[aux])->probability_table[builder]=1 - readerProb;
+		}
+	}
+}
 
 /**
  * Function to find hidden nodes in the joint
+ *
  * Attributes
  * ----------
  * Nodes : The map of nodes to add their Probabilities.
  * key_node : The key node.
  */
-string hiddenNodes(map<string, struct node*> nodes, string nodeKey){
-	string builder="";
-	if(nodes[nodeKey]->parents.empty()){
-		builder+=nodeKey;
-	}else{
-		for(int i= 0;i < nodes[nodeKey]->parents.size();i++){
-			builder+= hiddenNodes(nodes,nodes[nodeKey]->parents[i]->name) + " ";
+string hidden_nodes(map<string, struct node*> nodes, string key_node) {
+
+  string builder = "";
+
+  if(nodes[key_node]->parents.empty()) {
+		builder += key_node;
+	} else {
+		for(int i = 0; i < nodes[key_node]->parents.size(); i++) {
+			builder += hidden_nodes(nodes, nodes[key_node]->parents[i]->name) + " ";
 		}
-		builder+=nodeKey;
+		builder += key_node;
 	}
 	return builder;
 }
 
-
-/**
- * Function for the sumatory
- * Attributes
- * ----------
- * Buffer : To retrieve the input.
- * Relevant_nodes :
- * sum_expansion :
- */
-void expandSum(string buffer, vector<string> nodes, vector<string> &sumExp){
-	string builder1,builder2;
-	if(nodes.size()==1){
-		builder1 = buffer + ", +" + nodes[0];
-		sumExp.push_back(builder1);
-		builder2 = buffer + ", -" + nodes[0];
-		sumExp.push_back(builder2);
-	}else{
-		builder1 = buffer  + ", +" + nodes.back();
-		builder2 = buffer  + ", -" + nodes.back();
-		nodes.pop_back();
-		expandSum(builder1,nodes,sumExp);
-		expandSum(builder2,nodes,sumExp);
-	}
-}
-
 /**
  * Function for the chain rule
+ *
  * Attributes
  * ----------
  * Nodes : The map of nodes to add their Probabilities.
  * Chain_probability : The string probability of chain rule.
  */
-double chainRule(map<string, struct node*> nodes,string chainProb){
-	string builder, name;
-	char signs[]="+-";
-	std::vector<string> index,statements,auxilarStmnt,probs;
-	chainProb.erase (std::remove(chainProb.begin(), chainProb.end(), ','), chainProb.end());
-	stringstream extract(chainProb);
-	double acum=1.0;
-	while(extract >> builder){
+double chain_rule(map<string, struct node*> nodes,string chain_probability){
+
+  string builder, name;
+	char signs[] = "+-";
+	std::vector<string> index, statements, probs;
+  // To remove all ','
+	chain_probability.erase (std::remove(chain_probability.begin(), chain_probability.end(), ','), chain_probability.end());
+	stringstream extract(chain_probability);
+	double acum = 1.0;
+
+  while(extract >> builder) {
 		statements.push_back(builder);
-		for (int i = 0; i < strlen(signs); i++){
+		for (int i = 0; i < strlen(signs); i++) {
 	      builder.erase (std::remove(builder.begin(), builder.end(), signs[i]), builder.end());
 	  	}
 	  	index.push_back(builder);
 	}
-	for (int i = 0; i < index.size(); i++){
-	    //
-		std::map<string, double> copyMap = nodes[index[i]]->probabilityTable;
-		for (std::map<string,double>::iterator ite = copyMap.begin(); ite !=  copyMap.end(); ++ite){
+	for (int i = 0; i < index.size(); i++) {
+		std::map<string, double> copyMap = nodes[index[i]]->probability_table;
+		for (std::map<string,double>::iterator ite = copyMap.begin(); ite !=  copyMap.end(); ++ite) {
 			if (ite->first.find(statements[i]) != std::string::npos) {
 			    probs.push_back(ite->first);
 			}
 		}
-		if(!nodes[index[i]]->parents.empty()){
+		if(!nodes[index[i]]->parents.empty()) {
 			int dependeces = nodes[index[i]]->parents.size();
 			int indexProbs;
-			for(int j=0;j < probs.size();j++){
-				int count=0;
-				for (int k = 0; k < statements.size(); k++){
+			for(int j = 0; j < probs.size(); j++){
+				int count = 0;
+				for (int k = 0; k < statements.size(); k++) {
 					if(probs[j].find(statements[k]) != std::string::npos){
 						count++;
 					}
 				}
-				if(count == (dependeces+1)){
+				if(count == (dependeces + 1)) {
 					indexProbs = j;
 					break;
 				}
 			}
-			acum*=copyMap[probs[indexProbs]];
-
-		}else{
-			acum*=copyMap[probs[0]];
+			acum *= copyMap[probs[indexProbs]];
+		} else {
+			acum *= copyMap[probs[0]];
 		}
 		while(!probs.empty()){
 			probs.pop_back();
@@ -126,191 +230,165 @@ double chainRule(map<string, struct node*> nodes,string chainProb){
 }
 
 /**
+ * Function for the sumatory
+ *
+ * Attributes
+ * ----------
+ * Buffer : To retrieve the input.
+ * Relevant_nodes :
+ * sum_expansion :
+ */
+void sumatory_expand(string buffer, vector<string> relevant_nodes, vector<string> &sum_expansion) {
+
+  string builder1, builder2;
+
+  if(relevant_nodes.size() == 1) {
+		builder1 = buffer + ", +" + relevant_nodes[0];
+		sum_expansion.push_back(builder1);
+		builder2 = buffer + ", -" + relevant_nodes[0];
+		sum_expansion.push_back(builder2);
+	} else {
+		builder1 = buffer  + ", +" + relevant_nodes.back();
+		builder2 = buffer  + ", -" + relevant_nodes.back();
+		relevant_nodes.pop_back();
+		sumatory_expand(builder1, relevant_nodes, sum_expansion);
+		sumatory_expand(builder2, relevant_nodes, sum_expansion);
+	}
+}
+
+
+/**
  * Function to solve joint in the queries
+ *
  * Attributes
  * ----------
  * Nodes : The map of nodes to add their Probabilities.
  * Buffer : To retrieve the input.
  */
-double solve_joint(map<string, struct node*> nodes, string buffer){
-	string builder,aux,picker,hidden="";
-	vector<string> sumExp;
-	std::vector<string> relevantNodes;
-	double acum=0.0;
+double solve_joint(map<string, struct node*> nodes, string buffer) {
 
-	for(int i=0;i < buffer.length();i++){
-		if(buffer[i]==','){
-			aux = hiddenNodes(nodes,builder);
-			builder="";
+  string builder,aux,picker,hidden = "";
+	double acum = 0.0;
+
+  for(int i = 0; i < buffer.length(); i++) {
+		if(buffer[i] == ',') {
+			aux = hidden_nodes(nodes, builder);
+			builder = "";
 		}
-		if(buffer[i]=='+'||buffer[i]=='-'||buffer[i]==' '||buffer[i]==',');
+		if(buffer[i] == '+' || buffer[i] == '-' || buffer[i] == ' ' || buffer[i] == ',');
 		else{
-			builder+=buffer[i];
+      // To construct the string key for the map of nodes
+			builder += buffer[i];
 		}
+
 		stringstream extract(aux);
+
 		while(extract >> picker){
-			if(hidden.find(picker)== string::npos){
-				hidden+= picker + " ";
+			if(hidden.find(picker) != string::npos);
+			else{
+				hidden += picker + " ";
 			}
 		}
 	}
-	aux = hiddenNodes(nodes,builder);
-	builder="";
+	aux = hidden_nodes(nodes, builder);
+	builder = "";
 
 	stringstream extract1(aux);
+
 	while(extract1 >> picker){
-		if(hidden.find(picker)==string::npos){
-			hidden+= picker + " ";
-		}
-	}
-
-    //Hidden Nodes
-	stringstream extract2(hidden);
-	while(extract2 >> picker){
-		if(buffer.find(picker)!=string::npos);
+		if(hidden.find(picker) != string::npos);
 		else{
-			builder+= picker + " ";
-			relevantNodes.push_back(picker);
+			hidden += picker + " ";
 		}
 	}
 
-	if(!relevantNodes.empty()){
-		expandSum(buffer,relevantNodes,sumExp);
+	stringstream extract2(hidden);
 
-		for(int i=0;i<sumExp.size();i++){
-			acum+=chainRule(nodes,sumExp[i]);
+	std::vector<string> relevant_nodes;
+
+	while(extract2 >> picker){
+		if(buffer.find(picker) != string::npos);
+		else{
+			builder += picker + " ";
+			relevant_nodes.push_back(picker);
 		}
-	}else{
-		acum = chainRule(nodes,buffer);
+	}
+
+	vector<string> sum_expansion;
+
+	if(!relevant_nodes.empty()) {
+		sumatory_expand(buffer, relevant_nodes, sum_expansion);
+		for(int i = 0; i < sum_expansion.size(); i++) {
+			acum += chain_rule(nodes, sum_expansion[i]);
+		}
+	} else {
+		acum = chain_rule(nodes, buffer);
 	}
 	return acum;
 }
 
-int main(int argc, char *argv[]){
+/**
+ * Function to solve the queries in the input
+ * Attributes
+ * ----------
+ * Nodes : The map of nodes to add their Probabilities.
+ * Buffer : To retrieve the input.
+ */
+void solve_query(map<string, struct node*> nodes, string buffer){
+
+  double res;
+	string numerator, denominator;
+	numerator = "";
+	denominator = "";
+	bool dependece = false;
+
+  for(int i = 0; i < buffer.length(); i++){
+		if(buffer[i] == '|') {
+      // To know if has dependece
+			dependece = true;
+			numerator += ", ";
+		}
+		if(!dependece) {
+			if(buffer[i] != '|') {
+				numerator += buffer[i];
+			}
+		} else {
+			if(buffer[i] != '|') {
+				numerator += buffer[i];
+				denominator += buffer[i];
+			}
+		}
+	}
+
+	if(denominator.compare("") == 0){
+		res = solve_joint(nodes, numerator);
+	}else{
+		res = solve_joint(nodes,numerator) / solve_joint(nodes,denominator);
+	}
+
+	printf("%.5G\n", res);
+}
+
+
+int main(int argc, char *argv[]) {
+
 	std::map<string, struct node*> nodes;
-	string buffer, builder, bayesBuild, aux;
-	double probs;
+	string buffer, builder, aux;
 	char devourer;
-
+	double readerProb;
 	int sect = 0;
+
 	while(sect < 3){
-		getline(cin,buffer);
-		if(buffer.compare("[Nodes]")==0){
-    		//Create nodes
-        	getline(cin,buffer);
-        	for(int i=0;i < buffer.length();i++){
-        	    //Nodes separated by commas
-        		if(buffer[i]==','){
-        			nodes[builder] = new node;
-        			nodes[builder]->name = builder;
-        		}else if(i==(buffer.length()-1)){
-        			builder+=buffer[i];
-        			nodes[builder] = new node;
-        			nodes[builder]->name = builder;
-        		}else if(buffer[i]==' '){
-        			builder="";
-        		}else{
-        			builder+=buffer[i];
-        		}
-        	}
+		getline(cin, buffer);
+		if(buffer.compare("[Nodes]") == 0){
+			create_nodes(nodes, buffer);
 			sect++;
-		}else if(buffer.compare("[Probabilities]")==0){
-			//Create the Bayes Network
-        	while(buffer.compare("")!=0){
-        		getline(cin,buffer);
-        		bayesBuild="";
-        		//Child to parents
-        		bool indexNode=false;
-        		for(int i=0;i < buffer.length();i++){
-        			if(buffer[i]=='|'){
-        				indexNode = true;
-        				aux = bayesBuild;
-        				bayesBuild="";
-        			}
-        			if(buffer[i]=='+'||buffer[i]=='-'||buffer[i]==' '||buffer[i]=='='||buffer[i]==','||buffer[i]=='|'||(buffer[i]>=48&&buffer[i]<=57));
-        			else{
-        				bayesBuild+=buffer[i];
-        			}
-        			if(indexNode){
-        					if(buffer[i]==','){
-        						(nodes[aux])->parents.push_back(nodes[bayesBuild]);
-        						bayesBuild="";
-        					}else if (buffer[i]=='='){
-        						(nodes[aux])->parents.push_back(nodes[bayesBuild]);
-        						bayesBuild="";
-        					}
-        			}
-        			if(buffer[i]=='='){
-        				if(!indexNode){
-        					aux=bayesBuild;
-        				}
-        				break;
-        			}
-        		}
-        		//No Duplicate Nodes
-        		sort( (nodes[aux])->parents.begin(), (nodes[aux])->parents.end() );
-        		(nodes[aux])->parents.erase( unique( (nodes[aux])->parents.begin(), (nodes[aux])->parents.end() ), (nodes[aux])->parents.end() );
-
-        		//Builds the probability table
-        		bayesBuild="";
-        		for(int i=0;i < buffer.length();i++){
-        			if(buffer[i]=='='){
-        				break;
-        			}else if(buffer[i]==' '&&buffer[i+1]=='=');
-        			else{
-        				bayesBuild+=buffer[i];
-        			}
-        		}
-
-                //Assign probs
-        		if(bayesBuild.compare("")!=0){
-        			stringstream extract(buffer);
-        			do{
-        				extract >> devourer;
-        			}while(devourer!='=');
-
-
-        			extract >> probs;
-
-        			(nodes[aux])->probabilityTable[bayesBuild]=probs;
-
-        			bayesBuild[0]='-';
-        			(nodes[aux])->probabilityTable[bayesBuild]=1 - probs;
-        		}
-        	}
+		} else if(buffer.compare("[Probabilities]") == 0){
+			create_bayes_network(nodes, buffer);
 			sect++;
-
-		}else if (buffer.compare("[Queries]")==0){
-			while(getline(cin,buffer)){
-				//Solve the Query
-			  double res;
-        string numerator, denominator;
-        numerator="";
-        denominator="";
-        bool dependece=false;
-        for (int i = 0; i < buffer.length(); i++){
-        	if(buffer[i]=='|'){
-        		dependece=true;
-        		numerator+=", ";
-          }
-          if(!dependece){
-          	if(buffer[i]!='|'){
-          		numerator+=buffer[i];
-          	}
-          }else{
-          	if(buffer[i]!='|'){
-          		numerator+=buffer[i];
-          		denominator+=buffer[i];
-          	}
-          }
-        }
-        //Print the prob
-        if(denominator.compare("")==0){
-        	res = solve_joint(nodes,numerator);
-        }else{
-        	res = solve_joint(nodes,numerator) / solve_joint(nodes,denominator);
-        }
-        printf("%.5G\n", res);
+		}else if (buffer.compare("[Queries]") == 0){
+			while(getline(cin, buffer)){
+				solve_query(nodes, buffer);
 			}
 			sect++;
 		}
